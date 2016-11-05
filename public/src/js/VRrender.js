983af1a1e7af79eab97013846c473e06fc9c36f6
Mat1th <matthias@blackbox.local>
2016-11-05 23:22:33 +0100

    fix font path;

--- public/src/js/VRrender.js
+++ public/src/js/VRrender.js
@@ -1,11 +1,38 @@
 VR.render = (function () { //funtion to render a template.
-  var width = 0,
-    videoSize = 500;
-  var timelineData = {};
+  var _width = 0,
+    videoSize = 500,
+    timelineData = {};
   //render the template
   function init() {
     // r._getData();
+    console.log(_dragScroll());
     _getData();
+
+  }
+
+  function _dragScroll() {
+    console.log('sdfsdfdsf');
+    var curYPos = 0,
+      curXPos = 0,
+      curDown = false;
+    console.log('dagescsadf');
+    window.addEventListener('mousemove', function (e) {
+      if (curDown === true) {
+        console.log(curXPos);
+        window.scrollTo(document.body.scrollLeft + (curXPos - e.pageX), document.body.scrollTop + (curYPos - e.pageY));
+      }
+    });
+
+    window.addEventListener('mousedown', function (e) {
+      console.log(e);
+      curDown = true;
+      curYPos = e.pageY;
+      curXPos = e.pageX;
+    });
+
+    window.addEventListener('mouseup', function (e) {
+      curDown = false;
+    });
   }
 
 
@@ -18,32 +45,25 @@ VR.render = (function () { //funtion to render a template.
       timelineData = parsed;
       videoList.appendChild(_generateTimeline(parsed));
     });
-
-
-    // console.log(document.getElementsByClassName('.video-list')[0]);
-
-    // for(var i = 0; i < videoList.selectAllChildren(); i++) {
-    //     videoList.style.left = obj.timestamp + "px";
-    // }
   }
 
   function _generateTimeline(data) {
     let videoList = document.createElement('ul');
     data.videos.forEach(function (obj) {
-      width = width + videoSize;
+      _width = _width + videoSize;
 
       let element = _createVideoElement(obj.thumbnail, obj.title, obj.description, obj.datealias);
       videoList.innerHTML += element; //add it to the ul
 
     });
-    document.getElementsByClassName("video-list")[0].style.width = width + "px";
-    document.getElementById("timeline").style.width = width + 200 + "px";
+    document.getElementsByClassName('video-list')[0].style.width = _width + 'px';
+    document.getElementById('timeline').style.width = _width + 200 + 'px';
     return videoList;
   }
 
   function _createVideoElement(tumbUrl, title, description, date) {
     let tumbUrlGenerated = VR.get.urls(tumbUrl).tumbUrl;
-    let videoUrl = VR.get.urls(tumbUrl).tumbUrl;
+    // let videoUrl = VR.get.urls(tumbUrl).tumbUrl;
     let video = '<div class="video-wrapper" data-date="' + date + '">';
     video += '<img class="thumbnail" src="' + tumbUrlGenerated + '">';
     video += '  <div class="media-caption">';
@@ -70,47 +90,24 @@ VR.render = (function () { //funtion to render a template.
         VR.get.one('.loading').classList.add('disabled');
       }, delayTime); //add a delay because you won't see it with fast internet.
     }
-  };
-
-  function _dragScroll() {
-    var curYPos = 0,
-      curXPos = 0,
-      curDown = false;
-
-    window.addEventListener('mousemove', function (e) {
-      if (curDown === true) {
-        window.scrollTo(document.body.scrollLeft + (curXPos - e.pageX), document.body.scrollTop + (curYPos - e.pageY));
-      }
-    });
-
-    window.addEventListener('mousedown', function (e) {
-      console.log(e);
-      curDown = true;
-      curYPos = e.pageY;
-      curXPos = e.pageX;
-    });
-
-    window.addEventListener('mouseup', function (e) {
-      curDown = false;
-    });
   }
-  document.getElementsByClassName('video-play')[0].addEventListener('click', function () {
-      var videoElement = document.getElementsByTagName('video')[0];
-      videoElement.src = 'media/video/' + this.dataset.filename;
-      videoElement.style.display = 'block';
 
-      document.getElementsByClassName('thumbnail-wrapper')[0].style.display = 'none';
-    }) <<
-    << << < HEAD
-    // }
+
+
+  document.getElementsByClassName('video-play')[0].addEventListener('click', function () {
+    var videoElement = document.getElementsByTagName('video')[0];
+    videoElement.src = 'media/video/' + this.dataset.filename;
+    videoElement.style.display = 'block';
+
+    document.getElementsByClassName('thumbnail-wrapper')[0].style.display = 'none';
+  });
+  // }
   return {
     init: init
-  }; ===
-  === =
-  // }
+  };
+
   return {
     init: init,
     timelineData: timelineData
-  }; >>>
-  >>> > master
+  };
 })();
