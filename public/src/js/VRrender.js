34d4f6b2aff16d2e26de5e97d6244b36da982d12
Mat1th <matthias@blackbox.local>
2016-11-05 01:45:26 +0100

    add li to the ul list;

--- public/src/js/VRrender.js
+++ public/src/js/VRrender.js
@@ -2,14 +2,28 @@ VR.render = (function () { //funtion to render a template.
   //render the template
   function init() {
     // r._getData();
-    _getData()
-  };
+    _getData();
+  }
+
   function _getData() {
     VR.get.data('//127.0.0.1:8080/data/data.json').then(response => {
-      console.log(response);
-    })
+      _generateTimeline(JSON.parse(response));
+    });
   }
-  // show or hid the loading spinner
+
+  function _generateTimeline(data) {
+    let videoList = VR.get.one('.video-list');
+    data.videos.forEach(function (obj, index) {
+      console.log(obj);
+      let li = document.createElement('li');
+      let title = document.createElement('H1');
+      title.innerHTML = obj.title;
+      li.appendChild(title);
+      videoList.appendChild(li)
+    });
+  }
+
+  // show or hide the loading spinner
   function loading(show) {
     if (show) {
       VR.get.one('.loading').classList.remove('disabled')
